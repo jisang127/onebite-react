6c716bda9ff41217035637f3f8b7020b0d759e6b
sjpark <sjpark@tennod.com>
2026-08-18 14:34:46 +0900

    불필요

--- section05/src/components/Footer.jsx
+++ section05/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const myName = "박상지";
   return (
     <footer>
-      <h1>안녕 내 이름은 {myName}이야</h1>
+      <h2>안녕 내 이름은 {myName}이야</h2>
       <h1>footer</h1>
     </footer>
   );
