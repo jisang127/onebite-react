f229160f2933119b9a61d6f49adb7a41745353ff
sjpark <sjpark@tennod.com>
2026-08-21 13:54:01 +0900

    uesEffect 기본 내용 확인

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 // 간단한 회원가입 폼
 // 1. 이름
@@ -14,6 +14,15 @@ const Register = () => {
     bio: "",
   });
 
+  const refObj = useRef(0);
+  // console.log(refObj);
+
+  // 렌더링 중이 아니라 effect 안에서 접근 (OK)
+  useEffect(() => {
+    console.log(refObj.current);
+    console.log("Register 렌더링");
+  });
+
   const onChange = (e) => {
     console.log(e.target.name, e.target.value);
     // 새로운 객체 만들면서 프로퍼티의 키 자리에 대괄호 넣고 변수 넣으면 그 변수 값을 변경하겠다
@@ -24,6 +33,14 @@ const Register = () => {
 
   return (
     <div>
+      <button
+        onClick={() => {
+          refObj.current++;
+          console.log(refObj.current);
+        }}
+      >
+        ref +1
+      </button>
       <div>
         <input
           name="name"
