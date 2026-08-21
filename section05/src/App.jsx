cb9371acc53ddf30545e0f2eb5ab7ad5382842fa
sjpark <sjpark@tennod.com>
2026-08-21 13:31:45 +0900

    51.5.8 기본적인 회원관리 폼

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,23 +1,16 @@
 import "./App.css";
-import { useState } from "react";
+import Register from "./components/Register";
 
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
 // html 태그 반환(return)하는 것들 -> 컴포넌트
 // 보통 function명 따서 부름 => 여기서는 App 컴포넌트겠지
 // 컴포넌트로 쓰는 함수 첫글자는 무조건 대문자 -> 리액트는 소문자는 인정안함!
 
 function App() {
-  // [ 관리할 값, 해당 값 변화시킬 함수 ]
-  // const [count, setCount] = useState(1);
-  // console.log(count);
+  // 회원가입 폼
 
-  // 컴포넌트 값이 바뀌면 리렌더링으로 return값을 재 반환한다!
   return (
     <>
-      <Bulb />
-      {/* <h1>{light}</h1> */}
-      <Counter />
+      <Register />
     </>
   );
 }
---
+++ section05/src/components/Register.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+const Register = () => {
+  // default 속성 지정할 거면 input에 value={name} 반드시 지정해줘야함
+  const [name, setName] = useState("이름");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeBirth = (e) => {
+    setBirth(e.target.value);
+  };
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeBio = (e) => {
+    setBio(e.target.value);
+  };
+  //   console.log(name);
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        {/* {name} */}
+      </div>
+      <div>
+        <input value={birth} type="date" onChange={onChangeBirth} />
+        {birth}
+      </div>
+      <div>
+        <select value={country} onChange={onChangeCountry}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+        {country}
+      </div>
+      <div>
+        <textarea value={bio} onChange={onChangeBio} />
+        {bio}
+      </div>
+    </div>
+  );
+};
+
+export default Register;
